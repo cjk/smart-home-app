0cdc20beb8c4bedb7f86203b8836897d134acbf8
Claus Klingberg <cjk@pobox.com>
2016-03-13 15:28:50 +0100

    Make app more robust again unavailable fermenter-state.

--- src/common/fermenter/reducer.js
+++ src/common/fermenter/reducer.js
@@ -1,7 +1,7 @@
 import * as actions from './actions';
 import FermenterEnv from './fermenterEnv';
 import FermenterDev from './fermenterDev';
-import {List, Record, Map} from 'immutable';
+import { List, Record, Map } from 'immutable';
 
 const InitialState = Record({
   env: {},
@@ -12,7 +12,7 @@ const InitialState = Record({
 const initialState = new InitialState;
 
 // Note how JSON from server is revived to immutable record.
-const revive = ({currentState, envHistory}) => initialState.merge({
+const revive = ({ currentState, envHistory }) => initialState.merge({
   envHistory: List(envHistory),
   env: new FermenterEnv(currentState)
 });
@@ -34,6 +34,12 @@ export default function fermenterReducer(state = initialState, action) {
 
     case actions.FETCH_FERMENTER_STATE_SUCCESS: {
       const s = action.payload.fermenterState;
+
+      /* fermenter state is not available */
+      if (!s) {
+        return state;
+      }
+
       const env = new FermenterEnv(s.env);
       /* TODO: Map into FermenterDev-struct */
       const devices = s.devices;
