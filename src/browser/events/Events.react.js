225821956c51594c3f4d6c2716fefef18edb9697
Claus Klingberg <cjk@pobox.com>
2016-04-08 17:32:31 +0200

    Upgrade react-mdl and refactor datatable in events-list accordingly.

--- src/browser/events/Events.react.js
+++ src/browser/events/Events.react.js
@@ -1,5 +1,5 @@
 import Component from 'react-pure-render/component';
-import { DataTable } from 'react-mdl/lib';
+import { DataTable, TableHeader } from 'react-mdl/lib';
 import { List, Map } from 'immutable';
 import moment from 'moment';
 import React from 'react';
@@ -26,28 +26,34 @@ class EventList extends Component {
       { name: 'created', label: 'Time' },
       { name: 'action', label: 'Action' },
       { name: 'dest', label: 'Address' },
-      { name: 'src', label: 'From' },
-      { name: 'type', label: 'Type' },
+      //{ name: 'src', label: 'From' },
+      //{ name: 'type', label: 'Type' },
       { name: 'value', label: 'Value' },
     ]);
 
-    const rows = list.map(event => {
-      return columns.reduce((row, col) => {
+    const rows = list.map(event => (
+      columns.reduce((row, col) => {
         const colName = col.name;
         const content = (colName === 'created' ?
-                         moment(event[colName]).format('MMMM Do YYYY, HH:mm:ss') :
+                         moment(event[colName]).fromNow() :
                          event[colName]);
 
         return row.merge({ [colName]: content });
-      }, Map());
-    });
+      }, Map())
+    ));
 
     if (!list.size) return (
       <p><FormattedMessage {...messages.emptyList} /></p>
     );
 
     return (
-      <DataTable columns={columns.toJS()} rows={rows.toJS()} />
+      <DataTable rows={rows.toJS()}>
+        {
+          columns.map((col) => (
+            <TableHeader key={col.name} name={col.name}>{col.label}</TableHeader>
+          ))
+        }
+      </DataTable>
     );
   }
 }
