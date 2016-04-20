f43081c32e6ad55b2e933efcbc5c042bbf135676
Claus Klingberg <cjk@pobox.com>
2016-04-20 18:08:55 +0200

    Remove unneeded locales.

--- src/browser/main.js
+++ src/browser/main.js
@@ -11,14 +11,8 @@ import { addLocaleData } from 'react-intl';
 import { browserHistory } from 'react-router';
 import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
 
-import cs from 'react-intl/locale-data/cs';
 import de from 'react-intl/locale-data/de';
 import en from 'react-intl/locale-data/en';
-import es from 'react-intl/locale-data/es';
-import fr from 'react-intl/locale-data/fr';
-import no from 'react-intl/locale-data/no';
-import pt from 'react-intl/locale-data/pt';
-import ro from 'react-intl/locale-data/ro';
 
 // bluebirdjs.com/docs/why-bluebird.html
 window.Promise = Bluebird;
@@ -43,7 +37,7 @@ window.addEventListener('unhandledrejection', error => {
 });
 
 // github.com/yahoo/react-intl/wiki/Upgrade-Guide#add-call-to-addlocaledata-in-browser
-[cs, de, es, en, fr, no, pt, ro].forEach(addLocaleData);
+[de, en].forEach(addLocaleData);
 
 const store = configureStore({
   createEngine,
