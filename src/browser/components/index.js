d119b9134518992dc5b68a731516c2cde1af84fc
Claus Klingberg <cjk@pobox.com>
2017-03-14 13:58:44 +0100

    Cleanup some unused components from Este.

--- src/browser/components/index.js
+++ src/browser/components/index.js
@@ -8,8 +8,6 @@ export { default as FieldError } from './FieldError';
 export { default as Form } from './Form';
 export { default as Image } from './Image';
 export { default as Link } from './Link';
-export { default as Loading } from './Loading';
-export { default as SwitchTheme } from './SwitchTheme';
 export { default as Title } from './Title';
 export { default as View } from './View';
 export { default as focus } from './focus';
