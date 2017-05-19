ff8cca05bc5658a0cbac11bcc99113bce1e4377f
Claus Klingberg <cjk@pobox.com>
2017-05-19 12:24:13 +0200

    Cleanup what we won't need but leave src-folder for now

--- gulp/build.js
+++
@@ -1,3 +0,0 @@
-import gulp from 'gulp';
-
-gulp.task('build', ['build-webpack']);
--- gulp/jest.js
+++
@@ -1,8 +0,0 @@
-import childProcess from 'child_process';
-import gulp from 'gulp';
-
-gulp.task('jest', (done) => {
-  childProcess
-    .spawn('npm', ['run', 'jest'], { stdio: 'inherit' })
-    .on('close', done);
-});
