7d1df64c4511bdeda1a5cd9ca1609d9a65f7fbd6
Claus Klingberg <cjk@pobox.com>
2017-06-04 18:07:05 +0200

    WIP: Make fetching initial home-state server-/client-side work.

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,12 @@ import App from '../components/App';
 import Page from '../components/Page';
 
 import Button from 'material-ui/Button';
-
 import Typography from 'material-ui/Typography';
+
+import { bindActionCreators } from 'redux';
+import { Observable } from 'rxjs/Observable';
+import connectClient from '../lib/client';
+import { requestInitialStateSuccess } from '../lib/home/actions';
 
 const styles = {
   container: {
@@ -17,16 +21,43 @@ const styles = {
   },
 };
 
+// TODO: Refactor (out)?
+const reqInitState = client =>
+  Observable.create(observer => {
+    client.record.snapshot('knx/initialBusState', (error, record) => {
+      if (error) {
+        console.error('Error requesting initialBusState from deepstream');
+        observer.error('Error requesting initialBusState from deepstream');
+      }
+      observer.next(record);
+      observer.complete();
+    });
+    /* No cleanup necessary, we only requested a snapshot above */
+    return () => {};
+  });
+
 class Index extends React.Component {
   static async getInitialProps({ store, isServer }) {
-    console.log(`Dispatching connect client - on server?: ${isServer}`);
-    await store.dispatch(({ type: 'CONNECT_CLIENT' }: Action));
-    return { isServer }; // we can pass custom props to our component from here
-  }
+    console.log(`[getInitialProps] Dispatching connect client - on server?: ${isServer}`);
 
-  componentDidMount() {
-    console.log('Index page mounted! Home-props:');
-    console.log(this.props);
+    if (isServer) {
+      const state = await connectClient()
+        .connOpen()
+        .switchMap(client => reqInitState(client))
+        .take(1)
+      // DEBUGGING:
+      // .do(state => {
+      //   console.log(
+      //     `[getInitialProps] initial-state: ${JSON.stringify(state)}`
+      //   );
+      // })
+        .toPromise();
+
+      await store.dispatch(requestInitialStateSuccess(state));
+      return { livestate: state, isServer };
+    }
+
+    return { isServer };
   }
 
   render() {
@@ -46,5 +77,14 @@ class Index extends React.Component {
     );
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    reqInitialStateSuccess: bindActionCreators(
+      requestInitialStateSuccess,
+      dispatch
+    ),
+  };
+};
 
 export default withRedux(createStore)(Index);
