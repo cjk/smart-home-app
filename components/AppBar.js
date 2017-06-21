c37181a924da4d7fd1f9ffde8222451ff003a250
Claus Klingberg <claus.klingberg@daimler.com>
2017-06-21 16:43:19 +0200

    Add home-button to now fixed app-bar and improve design

--- components/AppBar.js
+++ components/AppBar.js
@@ -6,6 +6,8 @@ import { withStyles, createStyleSheet } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import HomeIcon from 'material-ui-icons/Home';
 import Typography from 'material-ui/Typography';
 
 type AppBarProps = {
@@ -16,9 +18,13 @@ const styleSheet = createStyleSheet('ButtonAppBar', {
   root: {
     position: 'relative',
     width: '100%',
+    marginBottom: 80,
   },
   appBar: {
-    position: 'relative',
+    // position: 'fixed',
+  },
+  homeButton: {
+    // color: theme.palette.text.icon[500],
   },
   flex: {
     flex: 1,
@@ -31,6 +37,9 @@ const MainAppBar = (props: AppBarProps) => {
     <div className={classes.root}>
       <AppBar className={classes.appBar}>
         <Toolbar>
+          <IconButton aria-label="Menu" className={classes.homeButton}>
+            <HomeIcon />
+          </IconButton>
           <Typography type="title" colorInherit className={classes.flex}>
             SmartHome
           </Typography>
