c70cad1429b3ab0c30ffc3cbddab6f282e65fdae
Claus Klingberg <cjk@pobox.com>
2017-11-08 11:50:22 +0100

    Refactor how environment-variables are loaded / used.
    Fixes undefined host-variable in production.

--- .next.config.js
+++ .next.config.js
@@ -1,21 +1,41 @@
 // This file is not going through babel transformation.
 // So, we write it in vanilla JS.
 // (But you could use ES2015 features supported by your Node.js version)
-const { assocPath } = require('ramda');
 
-const eslintRule = {
-  test: /\.js$/,
-  enforce: 'pre',
-  exclude: /node_modules/,
-  loader: 'eslint-loader',
-};
+/* eslint-disable global-require */
+const { IgnorePlugin } = require('webpack');
 
-module.exports = {
+require('dotenv').config();
+
+const initExport = {
   webpack: (config, { dev }) => {
-    // TODO: Figure out, how to render errors as warnings in dev mode.
-    // https://github.com/MoOx/eslint-loader/issues/174
-    if (dev) return config;
-    const rules = [].concat(eslintRule, config.module.rules);
-    return assocPath(['module', 'rules'], rules, config);
+    const prod = !dev;
+
+    config.plugins.push(new IgnorePlugin(/^\.\/locale$/, /moment$/));
+
+    if (dev) {
+      config.module.rules.push({
+        test: /\.(jsx?|gql|graphql)$/,
+        loader: 'eslint-loader',
+        exclude: ['/node_modules/', '/.next/', '/helper_scripts/'],
+        enforce: 'pre',
+      });
+    }
+
+    if (process.env.ANALYZE_BUILD) {
+      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: 8888,
+          openAnalyzer: true,
+        })
+      );
+    }
+
+    return config;
   },
 };
+
+/* eslint-enable global-require */
+module.exports = initExport;
